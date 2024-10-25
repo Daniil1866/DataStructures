75999944176e1d44bd85478b17bfdde13e6e2558
Daniil <daniilshahov33@gmail.com>
2024-10-25 21:46:13 +0300

    Stack

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+        Stack<string> stack = new Stack<string>();
+
+        //A LIFO data structure, useful in cases where you need to do undo/redo actions
+
+        stack.Push("item1");
+        stack.Push("item2");
+        stack.Push("item3");
+        stack.Push("item4");
+        stack.Push("item5");
+
+        while (stack.Count > 0)
+        {
+            System.Console.WriteLine(stack.Pop());
+        }
+    }
+}
